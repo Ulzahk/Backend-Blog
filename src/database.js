961d8ad1c098643fc57f65cd151ae76d8b063a70
Ulzahk <ulzahk@gmail.com>
2022-08-07 05:56:37 -0500

    [UPDATED] dependencies and database config

--- src/database.js
+++ src/database.js
@@ -4,16 +4,15 @@ const { dbUser, dbPassword, dbHost, dbName } = require('./config')
 
 const mongoDbUrl = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`
 
-const connection = async (cb) => {
+const connection = async () => {
   try {
     await mongoose.connect(mongoDbUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false
     })
     debug('MongoDB Connected')
   } catch (error) {
-    cb(error)
+    debug(error)
   }
 }
 
